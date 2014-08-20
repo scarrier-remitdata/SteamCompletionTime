5b67ddc53aace88314aafe9820a698c1e0eec4cc
Scott Carrier <scarrier@remitdata.com>
2014-08-19 22:50:14 -0500

    Initial check in

---
+++ SteamCompletionTime/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace SteamCompletionTime
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.steamIdTextBox = new System.Windows.Forms.TextBox();
+            this.getListButton = new System.Windows.Forms.Button();
+            this.resultsTextBox = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Steam User Id:";
+            // 
+            // steamIdTextBox
+            // 
+            this.steamIdTextBox.Location = new System.Drawing.Point(95, 9);
+            this.steamIdTextBox.Name = "steamIdTextBox";
+            this.steamIdTextBox.Size = new System.Drawing.Size(175, 20);
+            this.steamIdTextBox.TabIndex = 1;
+            // 
+            // getListButton
+            // 
+            this.getListButton.Location = new System.Drawing.Point(276, 9);
+            this.getListButton.Name = "getListButton";
+            this.getListButton.Size = new System.Drawing.Size(75, 23);
+            this.getListButton.TabIndex = 2;
+            this.getListButton.Text = "Get List";
+            this.getListButton.UseVisualStyleBackColor = true;
+            this.getListButton.Click += new System.EventHandler(this.GetListButton_Click);
+            // 
+            // resultsTextBox
+            // 
+            this.resultsTextBox.Location = new System.Drawing.Point(15, 35);
+            this.resultsTextBox.Multiline = true;
+            this.resultsTextBox.Name = "resultsTextBox";
+            this.resultsTextBox.ReadOnly = true;
+            this.resultsTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.resultsTextBox.Size = new System.Drawing.Size(444, 333);
+            this.resultsTextBox.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(471, 380);
+            this.Controls.Add(this.resultsTextBox);
+            this.Controls.Add(this.getListButton);
+            this.Controls.Add(this.steamIdTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Steam Games Completion Time";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox steamIdTextBox;
+        private System.Windows.Forms.Button getListButton;
+        private System.Windows.Forms.TextBox resultsTextBox;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ SteamCompletionTime/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Windows.Forms;
+using System.Xml;
+using RestSharp;
+
+namespace SteamCompletionTime
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void GetListButton_Click(object sender, EventArgs e)
+        {
+            var steamId = steamIdTextBox.Text;
+            if (string.IsNullOrEmpty(steamId))
+            {
+                resultsTextBox.Text = "Enter a steam id";
+                return;
+            }
+            var client = new RestClient("http://steamcommunity.com");
+            var response = client.Get(new RestRequest(string.Format("/id/{0}/games/?tab=all&xml=1",steamId)));
+            if (response.StatusCode != HttpStatusCode.OK)
+            {
+                resultsTextBox.Text = response.Content;
+                return;
+            }
+
+            var games = new Dictionary<string, GameInfo>();
+            using (XmlReader reader = XmlReader.Create(new StringReader(response.Content)))
+            {
+                while (reader.ReadToFollowing("name"))
+                {
+                    reader.Read();
+                    if (reader.NodeType == XmlNodeType.CDATA)
+                    {
+                        var gameName = reader.Value;
+                        gameName = Regex.Replace(gameName, @" - ", " ");//replace - with space
+                        gameName = Regex.Replace(gameName, @"[^a-zA-Z0-9 ,.-]", string.Empty); //strip non-ascii
+                        gameName = Regex.Replace(gameName, "Enhanced Edition", "", RegexOptions.IgnoreCase);
+                        gameName = Regex.Replace(gameName, "Game of the Year.*", "", RegexOptions.IgnoreCase);
+                        gameName = Regex.Replace(gameName, "GOTY.*", "", RegexOptions.IgnoreCase);
+                        if (!games.ContainsKey(gameName))
+                            games.Add(gameName, new GameInfo{Name = gameName});
+                    }
+                }
+            }
+            resultsTextBox.Text = string.Format("Loading {0} games", games.Keys.Count);
+            resultsTextBox.Refresh();
+            client = new RestClient("http://www.howlongtobeat.com");
+
+            foreach (var name in games.Keys.ToList())
+            {
+                var request = new RestRequest("/search_main.php?t=games&page=1&sorthead=&sortd=Normal&plat=&detail=0");
+                request.AddHeader("content-type", "application/x-www-form-urlencoded");
+                request.AddParameter("queryString", name, ParameterType.GetOrPost);
+                client.PostAsync(request, (asyncResponse, handle) => HandleResponse(games, name, asyncResponse));
+                Thread.Sleep(100);
+            }
+            while (games.Values.Any(g => !g.Retrieved))
+            {
+                Thread.Sleep(500);
+            }
+            var sortedList = games.Values.ToList().OrderByDescending(t => t.Hours).Select(t => t.Name + " - " + t.Hours);
+
+            resultsTextBox.Text = string.Join("\r\n", sortedList);
+        }
+
+        private void HandleResponse(Dictionary<string, GameInfo> games, string name, IRestResponse response)
+        {
+            games[name].Retrieved = true;
+            var doc = new HtmlAgilityPack.HtmlDocument();
+            doc.LoadHtml(response.Content);
+            var node = doc.DocumentNode.SelectSingleNode("//div[contains(@class, 'gamelist_tidbit time_')]");
+            if (node == null)
+                return;
+            var hours = node.InnerText;
+            hours = Regex.Replace(hours, " Hours", "");
+            hours = Regex.Replace(hours, "&#189;", ".5");
+            var hoursFloat = 0.0f;
+            float.TryParse(hours, out hoursFloat);
+            games[name].Hours = hoursFloat;
+        }
+
+    }
+
+    internal class GameInfo
+    {
+        public string Name { get; set; }
+        public float Hours { get; set; }
+        public bool Retrieved { get; set; }
+    }
+}
